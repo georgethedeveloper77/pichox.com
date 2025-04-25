bbd0b58d54519898b8bcfd8c851064f37ae74e44
georgethedeveloper <50877288+georgethedeveloper77@users.noreply.github.com>
2025-04-25 16:29:15 +0300

    .gitignore

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.playtubescript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | PlayTube - The Ultimate Video Sharing Platform
+// | Copyright (c) 2017 PlayTube. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost:3306";
+// MySQL Database User
+$sql_db_user = "pichoxuser";
+// MySQL Database Password
+$sql_db_pass = "ryA$%0jnYW0s7hmf";
+// MySQL Database Name
+$sql_db_name = "pichoxdb";
+
+// Site URL
+$site_url = "https://pichox.com"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "kenyanstream"; // Your purchase code, don't give it to anyone.
+
+$auto_redirect = true;
+
+$siteEncryptKey = "9d6f246bde5d39fb2710db5c771ecfabfdc9586e"; // Your site encrypt key, don't give it to anyone.
+
+?>
