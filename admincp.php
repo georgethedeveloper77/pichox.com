9f7202f40b964ff1dc881234e4449671c6d932ba
georgethedeveloper <50877288+georgethedeveloper77@users.noreply.github.com>
2025-04-30 17:14:45 +0300

    config, index.php

--- admincp.php
+++ admincp.php
@@ -2,8 +2,8 @@
 // +------------------------------------------------------------------------+
 // | @author georgethedeveloper
 // | @author_url 1: http://www.playtubescript.com
-// | @author_url 2: http://codecanyon.net/user/doughouzforest
-// | @author_email: wowondersocial@gmail.com   
+// | @author_url 2:  http://georgethedeveloper.com
+// | @author_email: info@georgethedeveloper.com   
 // +------------------------------------------------------------------------+
 // | PlayTube - The Ultimate Video Sharing Platform
 // | Copyright (c) 2017 PlayTube. All rights reserved.
